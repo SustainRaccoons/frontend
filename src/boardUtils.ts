3b16197a7aca18b93ee11b48d7b002fd08e9e8ba
Sneaky-GTH <awesomelulsheh@gmail.com>
2023-10-21 10:33:52 +0300

    initial util funcs

--- src/boardUtils.ts
+++ src/boardUtils.ts
@@ -1,4 +1,39 @@
-import { BoardState, Location } from "./types.ts";
+import { BoardState, Location, Piece } from "./types.ts";
+
+export function fullPieceMoves(piece: Piece, location: Location): Location[] {
+
+  let allowedMoves: Location[] = [];
+  switch (piece) {
+    case Piece.Pawn:
+      // pawn can theoretically move to any of the 3 squares in front of it or behind it based on its colour and if there is an enemy piece to the side
+      allowedMoves.push([location[0] - 1, location[1]]);
+      allowedMoves.push([location[0] - 1, location[1] + 1]);
+      allowedMoves.push([location[0] - 1, location[1] - 1]);
+
+      allowedMoves.push([location[0] + 1, location[1]]);
+      allowedMoves.push([location[0] + 1, location[1] + 1]);
+      allowedMoves.push([location[0] + 1, location[1] - 1]);
+
+      break;
+    
+    case Piece.Rook:
+      
+      break;
+
+    case Piece.Knight:
+      
+      break;
+
+    case Piece.Bishop:
+      
+      break;
+
+    default:
+      break;
+
+  }
+  return [];
+}
 
 export function getValidMoves(board: BoardState, position: Location): Location[] {
   return [];
