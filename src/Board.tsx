ecfdbccf931f275416c7755614a03b30984dd6bc
CodedSakura <sakuracoded@gmail.com>
2023-10-21 15:28:27 +0300

    improve piece selection logic

--- src/Board.tsx
+++ src/Board.tsx
@@ -9,7 +9,7 @@ import { BoardState, Location, Side, SidedPiece, sidedPieceToNotationMap, sidedP
 interface Props {
   side: Side;
   state: BoardState;
-  requestMove: (from: Location, to: Location) => any;
+  requestMove: (from: Location, to: Location) => boolean;
 }
 
 function makeFlatBoard(state: BoardState, side: Side): [ SidedPiece | null, Location ][] {
@@ -36,24 +36,30 @@ export default function Board({ side, state, requestMove }: Props) {
   const [ validMoves, setValidMoves ] = useState<Location[]>([]);
 
   const tileSelectHandler = (tileLoc: Location) => () => {
+    const piece = state[tileLoc[1]][tileLoc[0]];
     if (activePiece) {
-      requestMove(activePiece, tileLoc);
-      setActivePiece(null);
-      setValidMoves([]);
-    } else {
-      const piece = state[tileLoc[1]][tileLoc[0]];
+      if (requestMove(activePiece, tileLoc)) {
+        console.log("move success");
+        setActivePiece(null);
+        setValidMoves([]);
+        return;
+      } else console.log("move fail");
 
-      if (piece === null) {
+      if (piece === null ||
+            sidedPieceToSide(piece) !== side ||
+            (activePiece[0] === tileLoc[0] && activePiece[1] === activePiece[1])) {
+        setActivePiece(null);
+        setValidMoves([]);
         return;
       }
-
-      if (sidedPieceToSide(piece) !== side) {
-        return;
-      }
-
-      setActivePiece(tileLoc);
-      setValidMoves(getValidMoves(state, tileLoc));
     }
+
+    if (piece === null || sidedPieceToSide(piece) !== side) {
+      return;
+    }
+
+    setActivePiece(tileLoc);
+    setValidMoves(getValidMoves(state, tileLoc));
   };
 
   return <div>
@@ -68,10 +74,14 @@ export default function Board({ side, state, requestMove }: Props) {
                           [style.active]: activePiece !== null && (activePiece[0] === loc[0] && activePiece[1] === loc[1]),
                           [style.validMove]: validMoves.some(([ mx, my ]) => mx === loc[0] && my === loc[1]),
                         })}
-                        onClick={tileSelectHandler(loc)}>
+                        onMouseDown={tileSelectHandler(loc)}
+                        onDragStart={() => console.log(1)}
+                        draggable={true}
+                  >
                     <img
                           src={p !== null ? pieces[p] : emptyTile}
-                          alt={p !== null ? sidedPieceToNotationMap[p] : "empty"} />
+                          alt={p !== null ? sidedPieceToNotationMap[p] : "empty"}
+                          draggable={false} />
                   </div>)}
     </div>
     <button onClick={() => document.dispatchEvent(new Event("chess:swap"))}>Swap sides</button>
