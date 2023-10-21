8d8f66fa66c4ab7f2f09ebfdd8629ee9c346e810
CodedSakura <sakuracoded@gmail.com>
2023-10-21 10:01:08 +0300

    add board utils stub

---
+++ src/boardUtils.ts
@@ -0,0 +1,9 @@
+import { BoardState, Location } from "./types.ts";
+
+export function getValidMoves(board: BoardState, position: Location): Location[] {
+  return [];
+}
+
+export function isValidMove(board: BoardState, from: Location, to: Location): boolean {
+  return true;
+}
